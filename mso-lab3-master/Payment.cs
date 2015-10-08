ad127b8423a23b77211ff2417e46c73b28879e4a
Robin Sikkens <robinsikkens@live.nl>
2015-10-08 10:11:54 +0200

    Stuffs
    We were stupid!
    #WeTotallyShouldFeelLikeIdiotsThinkingOurDesignWasEvenRemotelyGood

---
+++ mso-lab3-master/Payment.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3
+{
+    abstract class Payment
+    {
+        void BeginTransaction(float Price){}
+        bool EndTransaction() { return true; }
+        void CancelTransaction() { }
+    }
+
+    class CashPayment : Payment
+    {
+        IKEAMyntAtare2000 coin;
+
+        public CashPayment()
+        {
+            coin = new IKEAMyntAtare2000();
+        }
+
+        public void BeginTransaction(float price)
+        {
+            coin.starta();
+            coin.betala((int)Math.Round(price * 100));
+        }
+
+        public bool EndTransaction()
+        {
+            coin.stoppa();
+            return true;
+        }
+
+        public void CancelTransaction()
+        {
+            coin.stoppa();
+        }
+    }
+
+    class CreditPayment : Payment
+    {
+        CreditCard c;
+        int id;
+
+        public CreditPayment()
+        {
+            c = new CreditCard();
+        }
+
+        public void BeginTransaction(float price)
+        {
+            c.Connect();
+            //50 cent extra charge for creditcards
+            id = c.BeginTransaction(price + 0.50f);
+        }
+
+        public bool EndTransaction()
+        {
+            return c.EndTransaction(id);
+        }
+
+        public void CancelTransaction()
+        {
+            c.CancelTransaction(id);
+        }
+    }
+
+    class DebitPayment : Payment
+    {
+        DebitCard d;
+        int id;
+
+        public DebitPayment()
+        {
+            d = new DebitCard();
+        }
+
+        public void BeginTransaction(float price)
+        {
+            d.Connect();
+            id = d.BeginTransaction(price);
+        }
+
+        public bool EndTransaction()
+        {
+            return d.EndTransaction(id);
+        }
+
+        public void CancelTransaction()
+        {
+            d.CancelTransaction(id);
+        }
+    }
+}
---
+++ mso-lab3-master/Ticket.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3
+{
+    class Ticket
+    {
+        public bool dueToday;
+        public string departure, destination;
+        public float price;
+        public UIClass ticketType;
+        public UIDiscount discount;
+
+        public Ticket()
+        {
+
+        }
+
+        public float CalculatePrice(string departure, string destination, UIClass type, UIDiscount discount)
+        {
+            //We have no reason to extend CalculatePrice to the pricecalculator class as Ticket is already the information expert
+            return PriceCalculator.Calculate(departure, destination, type, discount);
+        }
+    }
+
+    public static class PriceCalculator
+    {
+        public static float Calculate(string departure, string destination, UIClass type, UIDiscount discount)
+        {
+            float ret = 0;
+            int tariefeenheden = Tariefeenheden.getTariefeenheden(departure, destination);
+            switch (type)
+            {
+                case UIClass.FirstClass:
+                    ret = FirstClassCalculator.Calculate(tariefeenheden, discount);
+                    break;
+                case UIClass.SecondClass:
+                    ret = SecondClassCalculator.Calculate(tariefeenheden, discount);
+                    break;
+            }
+            return ret;
+        }
+    }
+
+    public static class FirstClassCalculator
+    {
+        public static float Calculate(int tariefeenheden, UIDiscount discount)
+        {
+            int col;
+            switch (discount)
+            {
+                default:
+                case UIDiscount.NoDiscount:
+                    col = 4;
+                    break;
+                case UIDiscount.TwentyDiscount:
+                    col = 5;
+                    break;
+                case UIDiscount.FortyDiscount:
+                    col = 6;
+                    break;
+            }
+
+            return PricingTable.getPrice(tariefeenheden, col);
+        }
+    }
+
+    public static class SecondClassCalculator
+    {
+        public static float Calculate(int tariefeenheden, UIDiscount discount)
+        {
+            int col;
+            switch (discount)
+            {
+                default:
+                case UIDiscount.NoDiscount:
+                    col = 1;
+                    break;
+                case UIDiscount.TwentyDiscount:
+                    col = 2;
+                    break;
+                case UIDiscount.FortyDiscount:
+                    col = 3;
+                    break;
+            }
+
+            return PricingTable.getPrice(tariefeenheden, col);
+        }
+    }
+}
